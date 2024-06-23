0d95f0aa0602520be27adb7b6a3e53991f01ed0c
Tom Kr. Bardal <tom@trada.no>
2024-06-23 19:08:37 +0200

    shields: seesaw wip

--- drivers/sensor/adafruit/seesaw/seesaw.c
+++ drivers/sensor/adafruit/seesaw/seesaw.c
@@ -28,13 +28,13 @@ static inline int seesaw_reboot(const struct device *dev)
 {
 	const struct seesaw_config *config = dev->config;
 
-	if (i2c_reg_update_byte_dt(&config->i2c, LSM6DS0_REG_CTRL_REG8,
-				   LSM6DS0_MASK_CTRL_REG8_BOOT,
-				   1 << LSM6DS0_SHIFT_CTRL_REG8_BOOT) < 0) {
-		return -EIO;
-	}
+	// if (i2c_reg_update_byte_dt(&config->i2c, LSM6DS0_REG_CTRL_REG8,
+	// 			   LSM6DS0_MASK_CTRL_REG8_BOOT,
+	// 			   1 << LSM6DS0_SHIFT_CTRL_REG8_BOOT) < 0) {
+	// 	return -EIO;
+	// }
 
-	k_busy_wait(USEC_PER_MSEC * 50U);
+	// k_busy_wait(USEC_PER_MSEC * 50U);
 
 	return 0;
 }
@@ -43,24 +43,24 @@ static inline int seesaw_reboot(const struct device *dev)
 static int seesaw_sample_fetch(const struct device *dev,
 				enum sensor_channel chan)
 {
-	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL ||
-			chan == SENSOR_CHAN_ACCEL_XYZ ||
-			chan == SENSOR_CHAN_GYRO_XYZ);
+	// __ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL ||
+	// 		chan == SENSOR_CHAN_ACCEL_XYZ ||
+	// 		chan == SENSOR_CHAN_GYRO_XYZ);
 
-	switch (chan) {
-	case SENSOR_CHAN_ACCEL_XYZ:
-		lsm6ds0_sample_fetch_accel(dev);
-		break;
-	case SENSOR_CHAN_GYRO_XYZ:
-		lsm6ds0_sample_fetch_gyro(dev);
-		break;
-	case SENSOR_CHAN_ALL:
-		lsm6ds0_sample_fetch_accel(dev);
-		lsm6ds0_sample_fetch_gyro(dev);
-		break;
-	default:
-		return -ENOTSUP;
-	}
+	// switch (chan) {
+	// case SENSOR_CHAN_ACCEL_XYZ:
+	// 	lsm6ds0_sample_fetch_accel(dev);
+	// 	break;
+	// case SENSOR_CHAN_GYRO_XYZ:
+	// 	lsm6ds0_sample_fetch_gyro(dev);
+	// 	break;
+	// case SENSOR_CHAN_ALL:
+	// 	lsm6ds0_sample_fetch_accel(dev);
+	// 	lsm6ds0_sample_fetch_gyro(dev);
+	// 	break;
+	// default:
+	// 	return -ENOTSUP;
+	// }
 
 	return 0;
 }
@@ -79,24 +79,24 @@ static int seesaw_channel_get(const struct device *dev,
 			       enum sensor_channel chan,
 			       struct sensor_value *val)
 {
-	struct seesaw_data *data = dev->data;
+	// struct seesaw_data *data = dev->data;
 
-	switch (chan) {
-	case SENSOR_CHAN_ACCEL_X:
-	case SENSOR_CHAN_ACCEL_Y:
-	case SENSOR_CHAN_ACCEL_Z:
-	case SENSOR_CHAN_ACCEL_XYZ:
-		lsm6dso_accel_channel_get(chan, val, data);
-		break;
-	case SENSOR_CHAN_GYRO_X:
-	case SENSOR_CHAN_GYRO_Y:
-	case SENSOR_CHAN_GYRO_Z:
-	case SENSOR_CHAN_GYRO_XYZ:
-		lsm6dso_gyro_channel_get(chan, val, data);
-		break;
-	default:
-		return -ENOTSUP;
-	}
+	// switch (chan) {
+	// case SENSOR_CHAN_ACCEL_X:
+	// case SENSOR_CHAN_ACCEL_Y:
+	// case SENSOR_CHAN_ACCEL_Z:
+	// case SENSOR_CHAN_ACCEL_XYZ:
+	// 	lsm6dso_accel_channel_get(chan, val, data);
+	// 	break;
+	// case SENSOR_CHAN_GYRO_X:
+	// case SENSOR_CHAN_GYRO_Y:
+	// case SENSOR_CHAN_GYRO_Z:
+	// case SENSOR_CHAN_GYRO_XYZ:
+	// 	lsm6dso_gyro_channel_get(chan, val, data);
+	// 	break;
+	// default:
+	// 	return -ENOTSUP;
+	// }
 
 	return 0;
 }
@@ -109,23 +109,23 @@ static const struct sensor_driver_api seesaw_api_funcs = {
 
 static int lsm6ds0_init_chip(const struct device *dev)
 {
-	const struct seesaw_config *config = dev->config;
-	uint8_t chip_id;
+	// const struct seesaw_config *config = dev->config;
+	// uint8_t chip_id;
 
-	if (seesaw_reboot(dev) < 0) {
-		LOG_DBG("failed to reboot device");
-		return -EIO;
-	}
+	// if (seesaw_reboot(dev) < 0) {
+	// 	LOG_DBG("failed to reboot device");
+	// 	return -EIO;
+	// }
 
-	if (i2c_reg_read_byte_dt(&config->i2c, LSM6DS0_REG_WHO_AM_I, &chip_id) < 0) {
-		LOG_DBG("failed reading chip id");
-		return -EIO;
-	}
-	if ((chip_id != LSM6DS0_VAL_WHO_AM_I) && (chip_id != LSM6DSOX_VAL_WHO_AM_I)) {
-		LOG_DBG("invalid chip id 0x%x", chip_id);
-		return -EIO;
-	}
-	LOG_DBG("chip id 0x%x", chip_id);
+	// if (i2c_reg_read_byte_dt(&config->i2c, LSM6DS0_REG_WHO_AM_I, &chip_id) < 0) {
+	// 	LOG_DBG("failed reading chip id");
+	// 	return -EIO;
+	// }
+	// if ((chip_id != LSM6DS0_VAL_WHO_AM_I) && (chip_id != LSM6DSOX_VAL_WHO_AM_I)) {
+	// 	LOG_DBG("invalid chip id 0x%x", chip_id);
+	// 	return -EIO;
+	// }
+	// LOG_DBG("chip id 0x%x", chip_id);
 
 	return 0;
 }
